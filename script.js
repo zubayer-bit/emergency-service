adaa8b2c0035ed6462767e22e3197e0db99fff18
zubayer-bit <zubayer766@gmail.com>
2025-08-28 06:19:56 -0700

    heart icon clicked

---
+++ script.js
@@ -0,0 +1,158 @@
+// heart icon clicked col-1 (first-col)
+document.getElementById('heart-icon-btn1').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+document.getElementById('heart-icon-btn2').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+document.getElementById('heart-icon-btn3').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+// heart icon clicked col-2 (second-col)
+document.getElementById('heart-icon-btn4').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+document.getElementById('heart-icon-btn5').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+document.getElementById('heart-icon-btn6').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+
+// heart icon clicked col-3 (third-col)
+document.getElementById('heart-icon-btn7').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+document.getElementById('heart-icon-btn8').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+document.getElementById('heart-icon-btn9').addEventListener('click', function(e){
+    e.preventDefault();
+    // console.log('heart icon clicked');
+    let availablePoint = parseInt(document.getElementById('available-point').innerText);
+
+    let count =  0;
+    // count = availablePoint + 1;
+    count = count + 1;
+    // const finalAvailablePoint = count + availablePoint;
+    availablePoint = availablePoint + count;
+    document.getElementById('available-point').innerText = availablePoint;
+    
+
+
+    
+})
+
+//-------------------------------------------heart icon clicked complete-------------------------------------------
+
+
